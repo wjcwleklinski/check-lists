fdabcdeb11b94b753dbb513038283f6c42cfe219
Wojciech Wlekliński <wjcwleklinski@gmail.com>
2021-04-11 15:24:03 +0200

    Initial commit

---
+++ src/main/java/com/wjcwleklinski/shoppingserver/ShoppingServerApplication.java
@@ -0,0 +1,13 @@
+package com.wjcwleklinski.shoppingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingServerApplication.class, args);
+	}
+
+}
