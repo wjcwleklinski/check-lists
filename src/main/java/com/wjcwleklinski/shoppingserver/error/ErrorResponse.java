e734adc314c3adda2fbed6d60bea3dfedb420725
Wojciech Wlekliński <wjcwleklinski@gmail.com>
2021-07-06 20:00:48 +0200

    Validation | refactor

--- src/main/java/com/wjcwleklinski/shoppingserver/error/ErrorResponse.java
+++ src/main/java/com/wjcwleklinski/shoppingserver/error/ErrorResponse.java
@@ -2,6 +2,8 @@ package com.wjcwleklinski.shoppingserver.error;
 
 import lombok.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 
@@ -19,13 +21,13 @@ public class ErrorResponse {
 
     private String path;
 
-    public static ErrorResponse buildErrorResponse(HttpStatus status, String message, String path) {
+    public static ErrorResponse buildErrorResponse(HttpStatus status, String message, WebRequest request) {
         return ErrorResponse.builder()
                 .timestamp(LocalDateTime.now())
                 .status(status.value())
                 .error(status.name())
                 .message(message)
-                .path(path)
+                .path(((ServletWebRequest) request).getRequest().getRequestURI())
                 .build();
     }
 }
