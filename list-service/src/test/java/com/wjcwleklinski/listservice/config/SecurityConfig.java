c4caca246da8a033127203c8e564226a8b6f4e32
Wojciech Wlekliński <wjcwleklinski@gmail.com>
2022-08-20 13:01:13 +0200

    Action fix

--- list-service/src/test/java/com/wjcwleklinski/listservice/config/SecurityConfig.java
+++ list-service/src/test/java/com/wjcwleklinski/listservice/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class TestSecurityConfig {
+public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
